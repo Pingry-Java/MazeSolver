3df0e4da8bf32b294684bd40ec21b71afcebdfe6
Mitchell Pavlak <mpavlak2018@pingry.org>
2017-05-18 13:00:51 -0400

    added import tags

--- MazeGui.java
+++ MazeGui.java
@@ -3,6 +3,15 @@ import javax.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
 public class MazeGui{
 	public static void main(String[] args){
 	/*
