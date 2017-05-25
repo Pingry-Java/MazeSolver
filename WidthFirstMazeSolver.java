9110f2221475bfa4ee23ef814c50b0e0bd27647f
Gikem361 <gmatthews2018@pingry.org>
2017-05-25 14:16:49 -0400

    Made changes to WidthFirstMazeSolver so that solwness factor works. Implemented checkpoint process where checkpoints are done in order.

--- WidthFirstMazeSolver.java
+++ WidthFirstMazeSolver.java
@@ -5,6 +5,11 @@ public class WidthFirstMazeSolver extends MazeSolution{
 	private ArrayList<Node> traveled;
 	private int[][] distance;
 	
+	/**
+	* This is the constructor for the Widthfirstmazesolver.
+	* @param m The maze that is to be solied
+	*/
+	
 	public WidthFirstMazeSolver(Maze m){
 		super(m);
 		
@@ -20,20 +25,46 @@ public class WidthFirstMazeSolver extends MazeSolution{
 	}
 	
 	public List<Node> path(){
+	
+	/*
+		if (super.getMaze().hasCheckpoints()){
 		
-		makeDistances(0, super.getMaze().getStart());
+			ArrayList<Node> l = new ArrayList<Node>();
+			ArrayList<Node> checkpoints = new ArrayList<Node>();
+			
+			checkpoints = super.getMaze().getCheckpoints();
+			
+			l.addAll(pointPath(super.getMaze().getStart(), checkpoints.get(0)));
+			
+			for (int i = 0; i < checkpoints.length()-1; i++){
+				l.addAll(pointPath(checkpoints.get(i), checkpoints.get(i+1)));
+			}	
+				
+			l.addAll(pointPath(checkpoints.get(checkpoints.length()-1), super.getMaze().getFinish()));
+			
+			return l;
+			
+		}
+		
+	*/
+		return pointPath(super.getMaze().getStart(), super.getMaze().getFinish());
+		
+	}
+	
+	public List<Node> pointPath(Node start, Node finish){
+		
+		makeDistances(0, start);
 		
 		ArrayList<Node> reversePath = new ArrayList<Node>();
 		ArrayList<Node> forwardPath = new ArrayList<Node>();
 		
-		reversePath = pathFinder(super.getMaze().getFinish());
+		reversePath = pathFinder(finish);
 		
 		for (Node n:reversePath){
 			forwardPath.add(0,n);
 		}
 		
-		return forwardPath;
-		
+		return forwardPath;	
 	}
 	
 	public void makeDistances(int last, Node n){
@@ -45,19 +76,19 @@ public class WidthFirstMazeSolver extends MazeSolution{
 		
 		if ((up!=null)&&(up.slowness()>=0)&&((findDistance(up)>last+1)||(findDistance(up)==-1))){
 			distance[up.getX()][up.getY()]=last+1;
-			makeDistances(last+1, up);
+			makeDistances(last+up.slowness(), up);
 		}
 		if ((right!=null)&&(right.slowness()>=0)&&((findDistance(right)>last+1)||(findDistance(right)==-1))){
 			distance[right.getX()][right.getY()]=last+1;
-			makeDistances(last+1, right);
+			makeDistances(last+right.slowness(), right);
 		}
 		if ((down!=null)&&(down.slowness()>=0)&&((findDistance(down)>last+1)||(findDistance(down)==-1))){
 			distance[down.getX()][down.getY()]=last+1;
-			makeDistances(last+1, down);
+			makeDistances(last+down.slowness(), down);
 		}
 		if ((left!=null)&&(left.slowness()>=0)&&((findDistance(left)>last+1)||(findDistance(left)==-1))){
 			distance[left.getX()][left.getY()]=last+1;
-			makeDistances(last+1, left);
+			makeDistances(last+left.slowness(), left);
 		}	
 	}
 	
@@ -75,31 +106,42 @@ public class WidthFirstMazeSolver extends MazeSolution{
 		Node left = super.getMaze().getNeighbors(n).get(0);
 		
 		
-		if ((up!=null)&&(findDistance(up) == findDistance(n)-1)){
+		if ((up!=null)&&(findDistance(up) > findDistance(n))){
 			l.addAll(pathFinder(up));
 			return l;
 		}
 		
-		if ((right!=null)&&(findDistance(right) == findDistance(n)-1)){
+		if ((right!=null)&&(findDistance(right) > findDistance(n))){
 			l.addAll(pathFinder(right));
 			return l;
 		}
 		
-		if ((down!=null)&&(findDistance(down) == findDistance(n)-1)){
+		if ((down!=null)&&(findDistance(down) > findDistance(n))){
 			l.addAll(pathFinder(down));
 			return l;
 		}
 		
-		if ((left!=null)&&(findDistance(left) == findDistance(n)-1)){
+		if ((left!=null)&&(findDistance(left) > findDistance(n))){
 			l.addAll(pathFinder(left));
 			return l;
 		}
 		return l;
 	}
 	
+	/**
+	* This method gets the int value of distance from the start Node from the 2D for the parameter
+	* @param n The node that the method gets the distance for
+	* @return Returns the int value representing the distance from the Node parameter to the start Node
+	*/
+	 
 	public int findDistance(Node n){
 		return distance[n.getX()][n.getY()];
 	}
+	
+	/**
+	* This metod prints the 2D array containing distances of the nodes on the path to the endpoint 
+	* @param arr the 2D array of distances of the nodes on the path to the endpoint 
+	*/
 	
 	public void printArray(int[][] arr){
 		for (int i = 0; i<arr.length; i++){
@@ -110,6 +152,14 @@ public class WidthFirstMazeSolver extends MazeSolution{
 				System.out.print(arr[i][j]);
 			}
 			System.out.println();
+		}
+	}
+	
+	public void distanceReset(){
+		for (int[] a : distance){
+			for (int b : a){
+				b = -1;
+			}
 		}
 	}
 }
