0c5d8620e0c0f540423b8dbfe6ebeb7c0228444b
Tom Drzik <tomdrzik@gmail.com>
2017-05-25 13:52:36 -0400

    Added brace needed to run

--- Maze.java
+++ Maze.java
@@ -118,6 +118,7 @@ public abstract class Maze implements Serializable, Checkpointable{
 				checkpoints.add(getNodeByCoords(randX, randY));
 			}
 		}
+    }
 
 	//Modifiers for start and finish
 	protected void setStart(Node x)
