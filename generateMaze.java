2b782d8620c9b1c493b5ee304ecd95f201534859
Drew Beckmen <dbeckmen>
2017-05-30 12:48:27 -0400

    cleaned up the recursive part of the algorithm.

--- generateMaze.java
+++ generateMaze.java
@@ -68,37 +68,20 @@ public class generateMaze extends Maze
 	 */
 	private void carveWalls(Node current)
 	{
+		((MazeSquare) current).setVisited(true);
 		MazeSquare next = eligible(current);
-		int x;
-		int y;
 		if(next == null)
 		{
-			x = 0;
-			y = 0;
-		}
-		else
-		{
-			x = next.getX();
-			y = next.getY();
+			return;
 		}
 		while(next != null)
 		{
-			((MazeSquare) (maze[x][y])).setSlowness(1);
-			((MazeSquare) (maze[x][y])).setVisited(true);
-			carveWalls(maze[x][y]); //recursive call 
+			next.setSlowness(1);
+			next.setVisited(true);
+			carveWalls(next);
 			next = eligible(current);
-			if(next == null)
-			{
-				x = 0;
-				y = 0;
-			}
-			else
-			{
-				x = next.getX();
-				y = next.getY();
-			}
 		}
-	} 
+	}
 	
 	/**
 	 * Looks for eligible neighbors, which is defined as a neighbor that has two unvisited neighbors
